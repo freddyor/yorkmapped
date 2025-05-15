d33028b25f2a1ab236108ed25e5546b8be6f1d50
freddyor <168146737+freddyor@users.noreply.github.com>
2025-05-15 05:25:56 +0100

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,7 @@
+// index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MapComponent from './components/MapComponent';
+
+// Render the React app into the root div
+ReactDOM.render(<MapComponent />, document.getElementById('root'));
